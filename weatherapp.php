979289f8a579147cd8186ce9fcc901b849d53a98
mindquaker <26782153+mindquaker@users.noreply.github.com>
2020-05-15 18:56:46 -0400

    Update weatherapp.php

--- weatherapp.php
+++ weatherapp.php
@@ -1,9 +1,11 @@
 <?php
 
-$key = "cc53c949f404f4064ac9b4e6c1e60c41";
+$key = "Your API KEY";
 $z = $_GET["zip"];
 $url  = "https://api.openweathermap.org/data/2.5/weather?zip=".$z.",us&units=imperial&appid=".$key;
 
+//this is the original link.
+//api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={your api key} 
 sleep(2.5);
 $fp = fopen ( $url , "r" );
 $contents = "";
