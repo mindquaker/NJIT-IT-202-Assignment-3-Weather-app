2ac6cc5884cd9d62a982263d7b7878648e8c6463
mindquaker <26782153+mindquaker@users.noreply.github.com>
2020-05-15 18:58:52 -0400

    Update weatherapp2.php

--- weatherapp2.php
+++ weatherapp2.php
@@ -1,8 +1,11 @@
 <?php
 
-$api = "cc53c949f404f4064ac9b4e6c1e60c41";
+$api = "Your API KEY.";
 $lat = $_GET["lat"];
 $long = $_GET["long"];
+
+//this is the original link.
+//api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={your api key}
 $url2  = "http://api.openweathermap.org/data/2.5/weather?lat=".$lat."&lon=".$long."&appid=".$api;
 sleep(2.5);
 //Your API Key has to be obtained from openweathermap.org
@@ -12,7 +15,6 @@ $contents = "";
 while ( $more = fread ( $fp, 1000  ) ) {
    $contents .=  $more ;
 }
-     //this overrides commented earlier hardcoded $data
 
 echo $contents ;
 
